e3554913d29baec7f8fcc3fb18619262a1d9c371
kamale1991 <shingo.mori.0104@gmail.com>
2022-08-28 16:42:56 +0900

    16bit/32bitのLFSR乱数を追加

--- math/src/random_bit.cpp
+++ math/src/random_bit.cpp
@@ -13,18 +13,33 @@ namespace math
 		{
 		}
 
-		int RandomBit::irbit_register_representation(uint32_t &iseed)
+		uint32_t RandomBit::irbit_register_representation(uint32_t &iseed)
 		{
 			uint32_t newbit;
 
-			newbit = (iseed & IB18) >> 17 ^ (iseed & IB5) >> 4 ^ (iseed & IB2) >> 1 ^ (iseed && IB1);
+			newbit = (iseed & IB18) >> 17 ^ (iseed & IB5) >> 4 ^ (iseed & IB2) >> 1 ^ (iseed & IB1);
 			iseed = (iseed << 1) | newbit;
-			return (int)newbit;
+			return newbit;
 		};
 
-		int RandomBit::irbit(uint32_t &iseed)
+		uint32_t RandomBit::irbit(uint32_t &iseed)
 		{
 			if (iseed & IB18)
+			{
+				std::cout << ((iseed ^ MASK) << 1) << std::endl;
+				iseed = ((iseed ^ MASK) << 1) | IB1;
+				return 1;
+			}
+			else
+			{
+				iseed <<= 1;
+				return 0;
+			}
+		}
+
+		uint16_t RandomBit16::irbit(uint16_t &iseed)
+		{
+			if (iseed & IB16)
 			{
 				iseed = ((iseed ^ MASK) << 1) | IB1;
 				return 1;
@@ -34,6 +49,23 @@ namespace math
 				iseed <<= 1;
 				return 0;
 			}
+		};
+
+		uint32_t RnadomBit32::irbit(uint32_t &iseed)
+		{
+			uint32_t bit;
+			bit = (iseed & IB2) ^ (iseed & IB3);
+			printf("%04X %d\n", iseed, bit);
+			return bit;
+		}
+
+		uint16_t RandomBit16::irbit2(uint16_t &iseed)
+		{
+			uint16_t bit;
+			bit = (iseed & 0x0001) ^ ((iseed & 0x0004) >> 2) ^ ((iseed & 0x0008) >> 3) ^ ((iseed & 0x0020) >> 5);
+			iseed = (iseed >> 1) | (bit << 15);
+			printf("%04X %d\n", iseed, bit);
+			return bit;
 		}
 
 	} // namespace random
--- math/test/random_bit.cpp
+++ math/test/random_bit.cpp
@@ -1,5 +1,6 @@
 #include "../include/random_bit.h"
 #include "gtest/gtest.h"
+#include <bitset>
 
 namespace math
 {
@@ -9,10 +10,50 @@ namespace math
 		{
 			RandomBit *rand;
 			uint32_t iseed = 1;
-			int res = rand->irbit1(iseed);
-			std::cout << "iseed: " << iseed << " result: " << res << std::endl;
+			uint32_t iseed2 = 1;
+			int bit, bit2;
+
+			for (uint32_t i = 0; i < 100; i++)
+			{
+				bit = rand->irbit(iseed);
+				bit2 = rand->irbit_register_representation(iseed2);
+				std::cout << "i: " << i << " (iseed,bit) = (" << iseed << "," << bit << ") (" << iseed2 << "," << bit2 << ")" << std::endl;
+			}
+			uint32_t x = 1;
+			std::cout << ((x << 1) | 1) << std::endl;
 			// EXPECT_EQ(a, golden);
 		}
+
+		TEST(RandomBit16, test)
+		{
+			RandomBit16 *rand16;
+			uint16_t iseed = 1;
+			std::bitset<16> bitset1 = iseed;
+			std::cout << bitset1 << std::endl;
+			uint16_t bit;
+			for (uint32_t i = 0; i < 100; i++)
+			{
+				bit = rand16->irbit(iseed);
+				bitset1 = iseed;
+				std::cout << bitset1 << std::endl;
+				std::cout << "i: " << i << " iseed: " << iseed << " bit: " << bit << std::endl;
+			}
+		}
+
+		TEST(RandomBit16, test2)
+		{
+			RandomBit16 *rand16;
+			uint16_t iseed = 1;
+			std::bitset<16> bitset;
+			for (int i = 0; i < 100; i++)
+			{
+				rand16->irbit2(iseed);
+				bitset = iseed;
+
+				std::cout << bitset << std::endl;
+			}
+		}
+
 	} // namespace random
 
 } // namespace math
