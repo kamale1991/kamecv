3537373e2f0f5de75293d8c3e43db4b20f8f4543
kamale1991 <shingo.mori.0104@gmail.com>
2022-08-28 21:03:40 +0900

    ポインタの初期化バグ修正

--- math/test/random_bit.cpp
+++ math/test/random_bit.cpp
@@ -8,7 +8,7 @@ namespace math
 	{
 		TEST(RandomBit16, test)
 		{
-			RandomBit16 *rand16;
+			RandomBit16 *rand16 = new RandomBit16();
 			uint16_t iseed = 1;
 			std::bitset<16> bitset1 = iseed;
 			std::cout << bitset1 << std::endl;
@@ -22,7 +22,7 @@ namespace math
 
 		TEST(RandomBit16, test2)
 		{
-			RandomBit16 *rand16;
+			RandomBit16 *rand16 = new RandomBit16();
 			uint16_t iseed = 1;
 			std::bitset<16> bitset;
 			for (int i = 0; i < 100; i++)
