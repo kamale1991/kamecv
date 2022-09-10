5ee2ed86b343f9f9719304759b786f869c4aec28
kamale1991 <shingo.mori.0104@gmail.com>
2022-09-10 16:18:07 +0900

    namespaceをmathだけにする

--- math/test/uniform_random.cpp
+++ math/test/uniform_random.cpp
@@ -3,26 +3,21 @@
 #include <vector>
 #include <numeric>
 
-namespace math
+using namespace math;
+
+TEST(UniformRandom, test)
 {
-	namespace random
+	UniformRandom *urand = new UniformRandom();
+	int32_t iseed = 1;
+	float result = 0.;
+	std::vector<float> vec;
+
+	for (int i = 0; i < 1000000; i++)
 	{
-		TEST(UniformRandom, test)
-		{
-			UniformRandom *urand = new UniformRandom();
-			int32_t iseed = 1;
-			float result = 0.;
-			std::vector<float> vec;
+		result = urand->frand(iseed);
+		vec.push_back(result);
+	}
 
-			for (int i = 0; i < 1000000; i++)
-			{
-				result = urand->frand(iseed);
-				vec.push_back(result);
-			}
-
-			float average = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
-			EXPECT_NEAR(0.5, average, 1.0e-4);
-		}
-	} // namespace random
-
-} // namespace math
+	float average = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
+	EXPECT_NEAR(0.5, average, 1.0e-4);
+}
